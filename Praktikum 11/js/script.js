21011746560fddde6756ade651644a73d3234a31
dimassaputra170 <114549557+dimassaputra170@users.noreply.github.com>
2022-12-14 10:22:13 +0700

    Praktikum11

---
+++ Praktikum 11/js/script.js
@@ -0,0 +1,47 @@
+document.querySelectorAll("a").forEach((a)=>{
+    // Jika di klik akan menjalankan fungsi computerChoice()
+    a.addEventListener("click", (element)=>{
+        computerChoice(element);
+    })
+})
+function computerChoice(element){
+    // pilihan user
+    let pilihanUser = element.target.innerText;
+
+    // menangkap elemen pilihan komputer
+    let pilihanKomputer = document.querySelector("#result");
+
+    // membuat pilihan komputer dengan array
+    let choices =["Rock", "Paper", "Scissors"];
+
+    // pilihan random untuk komputer
+    pilihanKomputer.innerHTML = choices[Math.round(Math.random()*2)];
+    pilihanKomputer = pilihanKomputer.innerHTML
+
+
+    // jika pilihan user == pilihan komputer (Draw)
+    if( pilihanUser == pilihanKomputer){
+        setTimeout(()=>alert("Draw"), 300);
+    }
+
+    //jika pilihan user yang menang
+    if(pilihanUser == "Rock" && pilihanKomputer == "Scissors"){
+        setTimeout(()=>alert("USER WIN"), 300);
+    }
+    else if(pilihanUser == "Paper" && pilihanKomputer == "Rock"){
+        setTimeout(()=>alert("USER WINN"), 300);
+    }
+    else if(pilihanUser == "Scissors" && pilihanKomputer == "Paper"){
+        setTimeout(()=>alert("USER WINNN"), 300);
+    }
+    //jika pilihan user yang kalah
+    if(pilihanUser == "Rock" && pilihanKomputer == "Paper"){
+        setTimeout(()=>alert("USER KALAH"), 300);
+    }
+    else if(pilihanUser == "Paper" && pilihanKomputer == "Scissors"){
+        setTimeout(()=>alert("USER KALAHH"), 300);
+    }  
+    else if(pilihanUser == "Scissors" && pilihanKomputer == "Rock"){
+        setTimeout(()=>alert("USER KALAHHH"), 300);
+    }  
+}
